6d6c83f2ea507744d1beb9e490371a1ffe84ffbb
jmarino13 <Jude@lmgmedicine.com>
2026-07-10 10:44:16 -0500

    Update availability.js

--- api/availability.js
+++ api/availability.js
@@ -1,31 +1,20 @@
 export default async function handler(req,res){
 
 res.setHeader("Access-Control-Allow-Origin","*");
-res.setHeader("Access-Control-Allow-Methods","GET, OPTIONS");
-
-if(req.method==="OPTIONS"){
- return res.status(200).end();
-}
-
 
 const BUSINESS_ID =
 "4a7bb2cd-2bfc-4bf9-aa0b-0fa508cc287b";
-
-
-const serviceId =
-"urn:blvd:Service:69d07ead-e607-4d63-ab54-644ae076b075";
 
 
 const query = `
 
 query {
 
-availability(
+cartBookableTimes(
 
 input:{
-locationId:"urn:blvd:Location:67044558-0bab-4c70-adc8-d7d627da6525"
 
-serviceId:"${serviceId}"
+cartId:"urn:blvd:Cart:63704ad3-3ece-44db-b11a-4f2af01c62ac"
 
 }
 
@@ -51,7 +40,6 @@ const response = await fetch(
 `https://sandbox.joinblvd.com/api/2020-01/${BUSINESS_ID}/client`,
 
 {
-
 method:"POST",
 
 headers:{
