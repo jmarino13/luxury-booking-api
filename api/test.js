b54c1294b04c7c4586991427260a8993cb403e39
jmarino13 <Jude@lmgmedicine.com>
2026-07-10 10:37:14 -0500

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,43 @@
+export default async function handler(req,res){
+
+const BUSINESS_ID =
+"4a7bb2cd-2bfc-4bf9-aa0b-0fa508cc287b";
+
+
+const query = `
+query {
+  __schema {
+    queryType {
+      fields {
+        name
+      }
+    }
+  }
+}
+`;
+
+
+const credentials =
+Buffer
+.from(process.env.BOULEVARD_API_KEY+":")
+.toString("base64");
+
+
+const response = await fetch(
+`https://sandbox.joinblvd.com/api/2020-01/${BUSINESS_ID}/client`,
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json",
+"Authorization":
+`Basic ${credentials}`
+},
+body:JSON.stringify({query})
+});
+
+
+const data = await response.json();
+
+res.status(200).json(data);
+
+}
