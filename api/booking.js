c7f52eabf595825324a79afb8058f14f6e7bf41f
jmarino13 <Jude@lmgmedicine.com>
2026-07-10 10:14:23 -0500

    Update booking.js

--- api/booking.js
+++ api/booking.js
@@ -1,5 +1,22 @@
 export default async function handler(req,res){
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 const BUSINESS_ID =
 "4a7bb2cd-2bfc-4bf9-aa0b-0fa508cc287b";
 
