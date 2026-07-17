38a76bc3543550c59f9ace9aa9abd0c443f1863d
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 15:52:34 -0500

    Update test.js

--- api/test.js
+++ api/test.js
@@ -2,47 +2,23 @@ export default async function handler(req,res){
 
 try {
 
-const apiKey = process.env.BOULEVARD_API_KEY;
-
-if (!apiKey) {
-  return res.status(500).json({
-    error:"Missing BOULEVARD_API_KEY"
-  });
-}
-
-
 const credentials =
 Buffer
-.from(apiKey + ":")
+.from(process.env.BOULEVARD_API_KEY + ":")
 .toString("base64");
 
 
-const query = `
-query {
-  business {
-    id
-    name
-  }
-}
-`;
-
-
 const response = await fetch(
-"https://api.joinblvd.com/api/2020-01/graphql",
+"https://api.joinblvd.com/api/2020-01/businesses",
 {
-method:"POST",
+method:"GET",
 headers:{
-"Content-Type":"application/json",
 "Authorization":`Basic ${credentials}`
-},
-body:JSON.stringify({
-query
-})
+}
 });
 
 
 const data = await response.json();
-
 
 return res.status(200).json(data);
 
