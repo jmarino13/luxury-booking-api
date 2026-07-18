b04624184b09340e8f3f94216fb6d0af63523025
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 23:49:16 -0500

    Update booking.js

--- api/booking.js
+++ api/booking.js
@@ -1,157 +1,54 @@
 export default async function handler(req,res){
 
-res.setHeader("Access-Control-Allow-Origin","*");
-res.setHeader("Access-Control-Allow-Methods","GET, OPTIONS");
-
 if(req.method==="OPTIONS"){
 return res.status(200).end();
 }
 
-
-const BUSINESS_ID =
-"4a7bb2cd-2bfc-4bf9-aa0b-0fa508cc287b";
-
+const BUSINESS_ID = process.env.BOULEVARD_BUSINESS_ID;
 
 const credentials =
 Buffer
-.from(process.env.BOULEVARD_API_KEY+":")
+.from(process.env.BOULEVARD_API_KEY + ":")
 .toString("base64");
 
 
-// CREATE CART
-
-const createCartQuery = `
-
+const query = `
 mutation {
-
-createCart(
-input:{
-locationId:"urn:blvd:Location:67044558-0bab-4c70-adc8-d7d627da6525"
-}
-){
-
-cart{
-
-id
-
-availableCategories{
-
-name
-
-availableItems{
-id
-name
-}
-
-}
-
-}
-
-}
-
-}
-
-`;
-
-
-
-const cartResponse = await fetch(
-
-https://api.joinblvd.com/api/2020-01/${BUSINESS_ID}/client
-
-{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json",
-"Authorization":
-`Basic ${credentials}`
-},
-
-body:JSON.stringify({
-query:createCartQuery
-})
-
-}
-
-);
-
-
-const cartData = await cartResponse.json();
-
-
-const cartId =
-cartData.data.createCart.cart.id;
-const serviceId =
-cartData.data.createCart.cart.availableCategories[0]
-.availableItems[0]
-.id;
-
-// GET AVAILABLE TIMES
-
-
-const today = new Date().toISOString().split("T")[0];
-
-
-const timesQuery = `
-query {
-
-cartBookableTimes(
-  id:"${cartId}"
-  locationId:"urn:blvd:Location:67044558-0bab-4c70-adc8-d7d627da6525"
-  searchDate:"${today}"
-  tz:"America/Chicago"
-){
-
-id
-startTime
-
-}
-
+ createCart(
+ locationId:"urn:blvd:Location:67044558-0bab-4c70-adc8-d7d627da6525"
+ ){
+  cart{
+   id
+   availableCategories{
+    name
+    availableItems{
+     id
+     name
+    }
+   }
+  }
+ }
 }
 `;
-const timesResponse = await fetch(
 
-`https://sandbox.joinblvd.com/api/2020-01/${BUSINESS_ID}/client`,
 
+const response = await fetch(
+`https://api.joinblvd.com/api/2020-01/${BUSINESS_ID}/client`,
 {
-
 method:"POST",
-
 headers:{
 "Content-Type":"application/json",
-"Authorization":
-`Basic ${credentials}`
+"Authorization":`Basic ${credentials}`
 },
-
 body:JSON.stringify({
-query:timesQuery
+query
 })
-
-}
-
-);
-
-
-
-const timesData =
-await timesResponse.json();
-
-
-
-res.status(200).json({
-
-cartId,
-
-serviceId,
-
-categories:
-cartData.data.createCart.cart.availableCategories,
-
-times:
-timesData
-
 });
+
+
+const data = await response.json();
+
+
+return res.status(200).json(data);
 
 }
