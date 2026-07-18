d90559c371792e0bcd96a7eb36effd701afa50c1
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 21:05:39 -0500

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,66 +1,10 @@
-export default async function handler(req,res){
-
-try {
-
-const key = process.env.BOULEVARD_API_KEY;
-
-if(!key){
-  return res.status(500).json({
-    error:"Missing BOULEVARD_API_KEY"
-  });
-}
-
-
-const credentials =
-Buffer
-.from(key + ":")
-.toString("base64");
-
-
-const query = `
-query {
- business {
-   id
-   name
- }
-}
-`;
-
-
-const BUSINESS_ID =
-"3a83c246-a294-4eee-9a1a-a960ade6528a";
-
-
 const response = await fetch(
-`https://dashboard.joinblvd.com/api/2020-01/${BUSINESS_ID}/client`,
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization":`Basic ${credentials}`
-},
-body:JSON.stringify({
-query
-})
-});
-
+"https://www.joinblvd.com"
+);
 
 const text = await response.text();
 
-
 return res.status(200).json({
-status:response.status,
-response:text
+status: response.status,
+response:text.substring(0,200)
 });
-
-
-}catch(error){
-
-return res.status(500).json({
-error:error.message,
-stack:error.stack
-});
-
-}
-
-}
