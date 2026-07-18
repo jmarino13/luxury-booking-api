6dc9ff4af1f54ef19d64172b1ed85030ae050bb8
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 22:51:22 -0500

    Update test.js

--- api/test.js
+++ api/test.js
@@ -22,11 +22,11 @@ const response = await fetch(
 "https://www.joinblvd.com/.api/graph",
 {
 method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization":`Basic ${credentials}`,
-"x-blvd-bid":"3a83c246-a294-4eee-9a1a-a960ade6528a"
-},
+headers: {
+  "Content-Type": "application/json",
+  "Authorization": `Bearer ${process.env.BOULEVARD_API_KEY}`,
+  "x-blvd-bid": process.env.BOULEVARD_BUSINESS_ID
+}
 body:JSON.stringify({
 query
 })
