5a58cd24d3e53fd73696d163233ecd5b2656297d
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 21:12:21 -0500

    Remove API handler implementation in test.js

--- api/test.js
+++ api/test.js
@@ -1,55 +1,12 @@
-export default async function handler(req,res){
-
-try {
-
-const key = process.env.BOULEVARD_API_KEY;
-
-const credentials =
-Buffer
-.from(key + ":")
-.toString("base64");
-
-
 const query = `
 query {
  business(id:"3a83c246-a294-4eee-9a1a-a960ade6528a") {
    id
    name
+   services {
+     id
+     name
+   }
  }
 }
 `;
-
-
-const response = await fetch(
-"https://www.joinblvd.com/b/.api/graph",
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization":`Basic ${credentials}`
-},
-body:JSON.stringify({
-query
-})
-});
-
-
-const text = await response.text();
-
-
-return res.status(200).json({
-status:response.status,
-response:text
-});
-
-
-}catch(error){
-
-return res.status(500).json({
-error:error.message,
-stack:error.stack
-});
-
-}
-
-}
