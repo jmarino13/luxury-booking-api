ccc487038f0aa5fe4088d158bd6ef518f50da2d4
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 23:01:22 -0500

    Refactor handler function in test.js

--- api/test.js
+++ api/test.js
@@ -2,10 +2,6 @@ export default async function handler(req,res){
 
 try {
 
-const key = process.env.BOULEVARD_API_KEY;
-
-
-
 const query = `
 mutation {
   createCart {
@@ -14,32 +10,28 @@ mutation {
 }
 `;
 
-
 const response = await fetch(
 "https://www.joinblvd.com/.api/graphql",
 {
 method:"POST",
 headers: {
-  "Content-Type": "application/json",
-  "Authorization": `Basic ${Buffer.from(
-    process.env.BOULEVARD_API_KEY + ":"
-  ).toString("base64")}`,
-  "x-blvd-bid": process.env.BOULEVARD_BUSINESS_ID
+"Content-Type":"application/json",
+"Authorization": `Basic ${Buffer.from(
+process.env.BOULEVARD_API_KEY + ":"
+).toString("base64")}`,
+"x-blvd-bid": process.env.BOULEVARD_BUSINESS_ID
 },
 body: JSON.stringify({
-  query
+query
 })
 });
 
-
 const text = await response.text();
-
 
 return res.status(200).json({
 status:response.status,
 response:text
 });
-
 
 }catch(error){
 
