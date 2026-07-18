77a5bedf224892e22f8343a424850405db3ee977
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 21:16:07 -0500

    Refactor GraphQL query to use __type for Business

--- api/test.js
+++ api/test.js
@@ -11,9 +11,10 @@ const credentials = Buffer
 
 const query = `
 query {
-  business(id:"3a83c246-a294-4eee-9a1a-a960ade6528a") {
-    id
-    name
+  __type(name:"Business") {
+    fields {
+      name
+    }
   }
 }
 `;
