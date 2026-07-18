d1767ce284ee712d63db119d2cde70f4b792515a
jmarino13 <Jude@lmgmedicine.com>
2026-07-17 23:21:01 -0500

    Refactor API handler to include credentials

--- api/test.js
+++ api/test.js
@@ -2,6 +2,13 @@ export default async function handler(req,res){
 
 try {
 
+const key = process.env.BOULEVARD_API_KEY;
+
+const credentials = Buffer
+  .from(key + ":")
+  .toString("base64");
+
+
 const query = `
 mutation {
   createCart {
@@ -10,26 +17,30 @@ mutation {
 }
 `;
 
+
 const response = await fetch(
 "https://www.joinblvd.com/.api/graph",
 {
 method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization": `Basic ${credentials}`,
-"x-blvd-bid": process.env.BOULEVARD_BUSINESS_ID
+headers: {
+  "Content-Type": "application/json",
+  "Authorization": `Basic ${credentials}`,
+  "x-blvd-bid": process.env.BOULEVARD_BUSINESS_ID
 },
-body:JSON.stringify({
-query: query
+body: JSON.stringify({
+  query
 })
 });
 
+
 const text = await response.text();
+
 
 return res.status(200).json({
 status:response.status,
 response:text
 });
+
 
 }catch(error){
 
